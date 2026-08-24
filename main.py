ce557782751ead42704dc28a78de0bbbfb8ef7fe
Mihail <Loshari4ek@gmail.com>
2026-08-24 15:14:45 +0300

    dg

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 
 print("Student Branching App")
 print("Проект для тренировки веток Git")
+
+# main.py после подключения report.py
+from profile import print_profile
+from subjects import print_subjects
+from report import print_report
+
+print("Student Branching App")
+print_profile()
+print_subjects()
+print_report()
